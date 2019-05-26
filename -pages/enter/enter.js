34b4ef828a35f33f4f927ac38281db49efa6578a
eamoses <e.a.moses@gmail.com>
2019-05-25 19:05:51 -0500

    both portfolios on one page

--- -pages/enter/enter.js
+++ -pages/enter/enter.js
@@ -1,8 +1,9 @@
 spa.page.enter = (META)=>{
     META.enterPage = true
     spa.buildPage(META).innerHTML += `
-      <div class='enter'>
-        <div class='button' onClick='spa.setPage("home")'>ENTER</div>
-      </div>
+      <iframe src="https://eamoses.github.io/index/" height="100%" width="50%" style="top:0;left:0;position:absolute;cursor:pointer;"></iframe>
+    <div class='enter'>
+      <div class='button' onClick='spa.setPage("home")'>OR ENTER HERE</div>
+    </div>
     `
 }
